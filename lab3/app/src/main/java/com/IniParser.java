b20aae0e7e94f1a9d8b1de6d85c5d2f239181828
egoarka <egoarka@gmail.com>
2018-12-09 19:48:23 +0300

    feat: rewrite to fp for fun))

--- lab3/app/src/main/java/com/IniParser.java
+++ lab3/app/src/main/java/com/IniParser.java
@@ -1,15 +1,16 @@
 package com;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Scanner;
-import java.util.logging.Logger;
-import java.util.regex.Matcher;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
+
+import com.spencerwi.either.Either;
+
+import io.vavr.Tuple;
+import io.vavr.collection.HashMap;
+import io.vavr.collection.List;
+import io.vavr.control.Try;
 
 public class IniParser {
   // @formatter:off
@@ -19,79 +20,82 @@ public class IniParser {
   
   // represtented in java like this
   private 
-    HashMap<
-      String,
-      HashMap<String, String>
-    > sections = new HashMap<>();
+   HashMap<String, HashMap<String, String>> state;
   // @formatter:on
 
-  private static Logger logger = MyLogger.getLogger(IniParser.class.getName());
-  private static Pattern sectionPattern = Pattern.compile("^\\[(.*)\\]$");
-  private static Pattern linePattern = Pattern.compile("^([^;\\[\\r\\n]*)");
+  private static String sectionPattern = "[\\[](.+)[\\]]";
+  private static String linePattern = "[^;\\[\\r\\n]+";
+  //@formatter:off
+  private static Pattern composedPattern = Pattern.compile(
+    String.format("^(%s)|(%s)$", 
+      linePattern,
+      sectionPattern
+    )
+  );
 
+  //@formatter:on  
   private IniParser() {
   }
 
-  public static IniParser fromFile(String filePath) {
-    logger.info("--- Start loading file...");
+  @SuppressWarnings("unchecked")
 
+  public static IniParser fromFile(String filePath) throws IOException {
     IniParser parser = new IniParser();
-
     File file = new File(filePath);
-    Scanner scr = null;
 
-    HashMap<String, String> currentSection = null;
+    // @formatter:off
+    parser.state = Files.lines(file.toPath())
+      .map(str -> 
+        composedPattern.matcher(str)
+          .results()
+          .map(r ->
+            // compute dumb null throwable regex >_>
+            Try.of(() ->
+              Either.either(
+                () -> List.of(r.group(0).split("=")).map(String::trim),
+                () -> r.group(3)
+              )
+            ).getOrNull()
+          )
+          .findAny()
+          .orElse(null)
+      )
+      .filter(r -> r != null) 
+      .reduce(
+        // counter, map
+        Tuple.of(
+          "",
+          HashMap.of("", HashMap.of("", ""))
+        ),
+        (state, item) -> {
+          String parent = state._1;
+          HashMap<String, HashMap<String, String>> sections = state._2;
+          return item
+            .fold(
+              line -> Tuple.of(
+                parent,
+                sections.put(parent, 
+                  sections.get(parent).getOrNull()
+                  .put(line.get(0), line.get(1))
+                )
+              ),
+              section -> Tuple.of(
+                // make this section the current
+                section,
+                // and add this section to map
+                sections.put(section,  
+                  HashMap.empty()
+                ) 
+              )
+            );
+        },
+        (a, b) -> a
+      )._2;
 
-    try {
-      scr = new Scanner(file);
-      scr.useDelimiter(System.lineSeparator());
-      while (scr.hasNext()) {
-        String l = scr.next();
-        /*
-         * section matcher
-         */
-        Matcher sectionMatcher = sectionPattern.matcher(l);
-        if (sectionMatcher.find() && sectionMatcher.group(1).length() > 0) {
-          String sectionName = sectionMatcher.group(1);
-          currentSection = new HashMap<String, String>();
-
-          parser.sections.put(sectionName, currentSection);
-
-          logger.info(String.format("Section: %s", sectionMatcher.group(1)));
-        } else {
-          /*
-           * line matcher
-           */
-          Matcher lineMatcher = linePattern.matcher(l);
-          if (lineMatcher.find() && lineMatcher.group(1).length() > 0) {
-            if (currentSection == null) {
-              throw new Error("section not found");
-            }
-
-            String line = lineMatcher.group(1);
-            List<String> pair = Arrays.asList(line.split("="))
-                .stream()
-                .map(String::trim)
-                .collect(Collectors.toList());
-
-            currentSection.put(pair.get(0), pair.get(1));
-
-            logger.info(String.format("Line: %s", lineMatcher.group(1)));
-          }
-        }
-
-      }
-    } catch (FileNotFoundException ex) {
-      logger.severe(ex.getMessage());
-    } finally {
-      scr.close();
-    }
-
-    logger.info("--- File has been loaded...");
     return parser;
   }
 
   public <T> T get(String section, String key) {
-    return (T) sections.get(section).get(key);
+    return (T) state.getOrElse(section, null).getOrElse(key, null);
   }
 }
--- lab3/app/src/test/java/com/IniParserTest.java
+++ lab3/app/src/test/java/com/IniParserTest.java
@@ -1,20 +1,117 @@
 package com;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
+import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+import org.hamcrest.CoreMatchers;
 import org.junit.Test;
+
+import io.vavr.Tuple;
+import io.vavr.Tuple2;
+import io.vavr.control.Try;
 
 public class IniParserTest {
   String projectDir = Paths.get("").toFile().getAbsolutePath();
   String filePath = String.join("/", projectDir, "input.ini");
+  private static String section = "[COMMON]";
+  private static String line = "Driver = libusb ; cypress / libusb / random / fileIQS";
+
+  private static String sectionPattern = "[\\[](.+)[\\]]";
+  private static String linePattern = "[^;\\[\\r\\n]+";
+  //@formatter:off
+  private static Pattern composedPattern = Pattern.compile(
+    String.format("^(%s)|(%s)$", 
+      linePattern,
+      sectionPattern
+    )
+  );
+  //@formatter:on  
+  @Test
+  public void streamNull() {
+    Stream.of(1).collect(Collectors.toList());
+  }
 
   @Test
-  public void simpleTest() {
+  public void tryTest() {
+    List<String> aaa = Arrays.asList("1", "21", "3");
+    Try.of(() -> aaa.get(4).split("=")).getOrNull();
+  }
+
+  @Test
+  public void simpleTest() throws IOException {
     IniParser p = IniParser.fromFile(filePath);
     assertTrue(p.get("COMMON", "DiskCachePath").equals("/sata/panorama"));
     assertTrue(p.get("LEGACY_XML", "ListenTcpPort").equals("1976"));
     assertTrue(p.get("DEBUG", "PlentySockMaxQSize").equals("126"));
+  }
+
+  // @formatter:off
+  public Map<String, String> transform(List<Tuple2<String, String>> pairs) {
+    return pairs
+      .stream()
+      .collect(
+        Collectors.toMap(
+          k -> k._1(),
+          v -> v._2() 
+        )
+      );
+  }
+
+  @Test
+  public void pureMap() {
+    // Map a = Map.of();
+    List<Tuple2<String, String>> pairs = 
+      Arrays.asList(
+        Tuple.of("key1", "value1"),
+        Tuple.of("key2", "value2")
+      );
+  }
+ 
+  @Test
+  public void listPairsToMapTest() {
+    List<Tuple2<String, String>> pairs = 
+      Arrays.asList(
+        Tuple.of("key1", "value1"),
+        Tuple.of("key2", "value2")
+      );
+
+    Map<String, String> mapped = transform(pairs);
+    List<String> keys = mapped.keySet().stream().collect(Collectors.toList());
+    List<String> values = mapped.values().stream().collect(Collectors.toList());
+
+    assertThat(Arrays.asList("key1", "key2"), CoreMatchers.is(keys));
+    assertThat(Arrays.asList("value1", "value2"), CoreMatchers.is(values));
+    // @formatter:on
+  }
+
+  @Test
+  public void regexSectionMatchTest() {
+    Optional<String> s = composedPattern.matcher(section)
+        .results()
+        .map(r -> r.group(3))
+        .findAny();
+
+    assertEquals("COMMON", s.get());
+  }
+
+  @Test
+  public void regexLineMatchTest() {
+    Optional<String> s = composedPattern.matcher(line)
+        .results()
+        .map(r -> r.group(0))
+        .findAny();
+
+    assertEquals("Driver = libusb".trim(), s.get().trim());
   }
 }
