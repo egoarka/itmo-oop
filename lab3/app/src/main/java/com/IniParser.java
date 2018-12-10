dd19d025da69f716a2b8265f3cf0294c7e6abe7d
egoarka <egoarka@gmail.com>
2018-12-10 17:54:38 +0300

    chore: apply prettier, ps. looks much better

--- lab3/app/src/main/java/com/IniParser.java
+++ lab3/app/src/main/java/com/IniParser.java
@@ -14,88 +14,70 @@ import io.vavr.control.Try;
 
 public class IniParser {
   // @formatter:off
-  // internal structure of ini parser 
-  // consist of tuples (line's key, line's value) 
+  // internal structure of ini parser
+  // consist of tuples (line's key, line's value)
   // keyed by string (section key)
-  
   // represtented in java like this
-  private 
-   HashMap<String, HashMap<String, String>> state;
+  private HashMap<String, HashMap<String, String>> state;
   // @formatter:on
 
   private static String sectionPattern = "[\\[](.+)[\\]]";
   private static String linePattern = "[^;\\[\\r\\n]+";
   //@formatter:off
   private static Pattern composedPattern = Pattern.compile(
-    String.format("^(%s)|(%s)$", 
-      linePattern,
-      sectionPattern
-    )
+    String.format("^(%s)|(%s)$", linePattern, sectionPattern)
   );
 
-  //@formatter:on  
+  //@formatter:on
   private IniParser() {
   }
 
   @SuppressWarnings("unchecked")
-
   public static IniParser fromFile(String filePath) throws IOException {
     IniParser parser = new IniParser();
     File file = new File(filePath);
 
     // @formatter:off
-    parser.state = Files.lines(file.toPath())
-      .map(str -> 
-        composedPattern.matcher(str)
-          .results()
-          .map(r ->
-            // compute dumb null throwable regex >_>
-            Try.of(() ->
-              Either.either(
-                () -> List.of(r.group(0).split("=")).map(String::trim),
-                () -> r.group(3)
-              )
-            ).getOrNull()
+    parser.state = Files.lines(file.toPath()).map(
+      str -> composedPattern.matcher(str).results().map(
+        r -> // compute dumb null throwable regex >_>
+          Try.of(
+            () -> Either.either(
+              () -> List.of(r.group(0).split("=")).map(String::trim),
+              () -> r.group(3)
+            )
+          ).getOrNull()
+      ).findAny().orElse(null)
+    ).filter(r -> r != null).reduce(
+      // counter, map
+      Tuple.of("", HashMap.of("", HashMap.of("", ""))),
+      (state, item) -> {
+        String parent = state._1;
+        HashMap<String, HashMap<String, String>> sections = state._2;
+        return item.fold(
+          line -> Tuple.of(
+            parent,
+            sections.put(
+              parent,
+              sections.get(parent).getOrNull().put(line.get(0), line.get(1))
+            )
+          ),
+          section -> Tuple.of(
+            // make this section the current
+            section,
+            // and add this section to map
+            sections.put(section, HashMap.empty())
           )
-          .findAny()
-          .orElse(null)
-      )
-      .filter(r -> r != null) 
-      .reduce(
-        // counter, map
-        Tuple.of(
-          "",
-          HashMap.of("", HashMap.of("", ""))
-        ),
-        (state, item) -> {
-          String parent = state._1;
-          HashMap<String, HashMap<String, String>> sections = state._2;
-          return item
-            .fold(
-              line -> Tuple.of(
-                parent,
-                sections.put(parent, 
-                  sections.get(parent).getOrNull()
-                  .put(line.get(0), line.get(1))
-                )
-              ),
-              section -> Tuple.of(
-                // make this section the current
-                section,
-                // and add this section to map
-                sections.put(section,  
-                  HashMap.empty()
-                ) 
-              )
-            );
-        },
-        (a, b) -> a
-      )._2;
+        );
+      },
+      (a, b) -> a
+    )._2;
 
     return parser;
   }
 
   public <T> T get(String section, String key) {
-    return (T) state.getOrElse(section, null).getOrElse(key, null);
+    return (T) state.getOrElse(section, HashMap.empty()).getOrElse(key, "");
   }
+
 }
